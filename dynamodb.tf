0e06a7fa715d6597b641368faa8c3bead9b731bc
Joachim Rørbøl <2394155+rorbol@users.noreply.github.com>
2024-04-15 16:04:51 +0200

    Initial AWS S3 backend w dynamo lock (#1)

---
+++ dynamodb.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "this" {
+  name         = "${local.name_prefix}${var.name}-tfstate-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.this.arn
+  }
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = local.default_tags
+}
