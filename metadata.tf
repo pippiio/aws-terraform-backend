1b6824327f0951f57b76e839a5f26e36972a62a9
Joachim Rørbøl <2394155+rorbol@users.noreply.github.com>
2025-02-15 22:16:14 +0100

    feat: Added metadata variables

--- metadata.tf
+++ metadata.tf
@@ -1,1 +1,14 @@
-../metadata.tf
+variable "metadata" {
+  description = "JSON encoded metadata."
+  type        = string
+  nullable    = false
+
+  validation {
+    error_message = "Metadata is not a valid json string."
+    condition     = var.metadata == null || can(jsondecode(var.metadata))
+  }
+}
+
+locals {
+  metadata = jsondecode(var.metadata)
+}
