c3022528c0f1567961772b1a3ca7611639b04202
Joachim Rørbøl <2394155+rorbol@users.noreply.github.com>
2026-02-14 23:48:25 +0100

    feat!: replaced dynamodb with lock file

--- output.tf
+++ output.tf
@@ -2,22 +2,17 @@ output "backend" {
   value = { for tfstate in var.tfstate : tfstate => <<-EOL
     terraform {
       backend "s3" {
-        bucket         = "${aws_s3_bucket.this.bucket}"
-        key            = "${tfstate}.tfstate"
-        region         = "${local.region_name}"
-        encrypt        = true
-        dynamodb_table = "${aws_dynamodb_table.this[tfstate].name}"
+        bucket              = "${aws_s3_bucket.this.bucket}"
+        key                 = "${tfstate}.tfstate"
+        region              = "${local.region_name}"
+        encrypt             = true
+        use_lockfile        = true
         allowed_account_ids = ["${local.account_id}"]
 
         # assume_role = {
         #   role_arn     = "arn:aws:iam::${local.account_id}:role/OrganizationAccountAccessRole"
         #   session_name = "Terraform"
         # }
-
-        # Local apply only - DO NOT commit to git
-        # access_key          = ""
-        # secret_key          = ""
-        # token               = ""
       }
     }
   EOL
